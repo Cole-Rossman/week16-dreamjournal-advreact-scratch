7184233eb4f79888a76cb598a2ac89d2220f392a
Cole Rossman <colerossman99@gmail.com>
2022-05-17 16:43:14 -0700

    BrowserRouter and UserProvider added to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { UserProvider } from './context/UserContext';
+import { BrowserRouter } from 'react-router-dom';
 
 render(
   <React.StrictMode>
+    <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
