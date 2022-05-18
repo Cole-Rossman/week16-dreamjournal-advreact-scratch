6e323db4c421cc950779e10b856666d045716947
Cole Rossman <colerossman99@gmail.com>
2022-05-17 17:58:41 -0700

    views changed to add more pages for user to visit, auth.jsx in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { useUser } from './context/UserContext';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Header from './components/Header/Header';
+import Auth from './views/Auth/Auth';
+import Home from './views/Home/Home';
+import AddEntry from './views/JournalEntries/AddEntry';
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const { currentUser } = useUser();
+
+  return (
+    <>
+    {currentUser && <Header/>}
+    <Switch>
+      <Route path="/login">
+        {!currentUser ? <Auth /> : <Redirect to="/" /> }
+      </Route>
+      {/* finish all other routes */}
+    </Switch>
+    </>
+  );
 }
